03f1fd1f4d4e76cde7b1034a48e173ac27cb08bd
Tira <217bssr@gmail.com>
2017-09-24 02:40:26 +0000

    06_実習の実施

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,5 @@
 class CustomersController < ApplicationController
-  before_action :authenticate_url!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @q = Customer.search(params[:q])
