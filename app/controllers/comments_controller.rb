ff97cb13ea0c1b3b5e9636b5d2e65a64bda34e1e
Tira <217bssr@gmail.com>
2017-09-24 12:19:32 +0000

    08_基本課題の実施(2)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,12 +10,11 @@ class CommentsController < ApplicationController
     end
     def create
         @comment = Comment.new(comment_params)
- @comment.save
- #       if @comment.save
-            redirect_to customer_path(@comment.customer_id)
-#        else
-#            redirect_to customer_path(@comment.customer_id)
-#        end
+        if @comment.save
+          redirect_to customer_path(@comment.customer_id)
+        else
+          redirect_to customer_path(@comment.customer_id), flash: { error_message: @comment.errors.full_messages, error_count: @comment.errors.count, error_comment: @comment.body }
+        end
     end
     def edit
     
