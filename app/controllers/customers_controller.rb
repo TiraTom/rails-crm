dc25a53dd465c0b060892c82f9843b021631afbc
Tira <217bssr@gmail.com>
2017-09-24 12:42:01 +0000

    09_基本課題(1)の実施

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,5 @@
 class CustomersController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:index, :new, :create, :edit, :update, :show, :destroy]
   before_action :find_customer, only: [:show, :edit, :destroy, :update]
 
   def index
